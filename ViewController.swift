baaa039bc2e5407b3f13e7fc622902a68e1bd3e7
GabeLops <gabrieldlops@gmail.com>
2019-12-02 00:13:47 -0800

    added more site restrictions

--- ViewController.swift
+++ ViewController.swift
@@ -57,7 +57,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
         
         guard let actionTitle = action.title else{return}
         guard let url = URL(string: "https://" + actionTitle) else {return}
-        if actionTitle != "u.gg" && actionTitle != "google.com" {
+        if actionTitle != "u.gg" {
         webView.load(URLRequest(url: url))
         }else{
             let stopLoad = UIAlertController(title: "Invalid Page", message: "The page you are trying to reach is blocked", preferredStyle: .alert)
@@ -82,15 +82,21 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
         let url = navigationAction.request.url
+        var urlAllowed = true
         
         if let host = url?.host {
             for website in websites {
                 if host.contains(website){
                     decisionHandler(.allow)
                     return
-                }
+                }else {urlAllowed = false}
             }
         }
         decisionHandler(.cancel)
+        if urlAllowed == false {
+            let blockedAC = UIAlertController(title: "Error", message: "This website is not allowed.", preferredStyle: .alert)
+            blockedAC.addAction(UIAlertAction(title: "Continue", style: .default))
+            present(blockedAC, animated: true)
+        }
     }
 }
